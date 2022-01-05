bbb11e2d2506bf419a1cbae8c5b8b23b345be230
Christian Frohn <50671696+vFrohn@users.noreply.github.com>
2022-01-05 21:41:22 +0100

    Update GetMailboxInboxPermissions.ps1

--- GetMailboxInboxPermissions.ps1
+++ GetMailboxInboxPermissions.ps1
@@ -3,5 +3,14 @@ $Mailboxes = Get-Mailbox -RecipientTypeDetails UserMailbox
 Foreach ($mailbox in $Mailboxes)
 {
     $UserName = $Mailbox.Identity
-    Get-MailboxFolderPermission $UserName ':\Inbox' | where {$_.user.tostring() -ne "Default" } | Select-Object Identity, User,FolderName,AccesRights
+    Get-MailboxFolderPermission $UserName':\Inbox' | where {$_.user.tostring() -ne "Default" } | Select-Object Identity, User,FolderName,AccesRights
+}
+
+
+$Mailboxes = Get-Mailbox
+
+Foreach ($mailbox in $Mailboxes)
+{
+    Get-MailboxPermission $Mailbox.Identity | where {$_.user.tostring() -ne "NT AUTHORITY\SELF" -and $_.IsInherited -eq $false} | Export-Csv C:\Temp -NoTypeInformation -Encoding Unicode -Append
+
 }
